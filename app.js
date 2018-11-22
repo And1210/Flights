52afa441a6bdf15d05ed64ef095f4b9ba9a048b1
And1210 <amf7crazy@gmail.com>
2018-11-22 01:59:03 -0500

    Reset

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const TravelPayouts = require('travelpayouts-api');
 
 var app = express();
-var flightAPI = new TravelPayouts('a264e783f313166fe9a86b290ac84929', '169888');
 
 app.get('/', function(req, res) {
-    flightAPI.search({
-        origin: 'YYZ',
-        destination: 'MUC',
-        date: new Date()
-    }).then(function (data) {
-        console.log(data);
-    });
     res.send("hello");
 });
 
--- content.js
+++
@@ -1,3 +0,0 @@
-var results = document.getElementsByClassName("gws-flights-results__result-item gws-flights__flex-box gws-flights-results__collapsed");
-
-console.log(results);
